5d9565ae830854ee7cb3cad55da14e0892e326f6
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-08 21:36:59 +0000

    Display request json in page

---
+++ html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <script src="assets/u2f-api.js"></script>
+    <script>
+      var request = <?= $request ?>;
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
--- register.php
+++ register.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once 'src/functions.php';
+
 spl_autoload_register(function ($class_name) {
     $filename = basename(str_replace('\\', '/', $class_name)).'.php';
     include 'src/'.$filename;
 });
+
+
+use Firehed\U2F\Server;
+
+$server = new Server();
+$server->disableCAVerification()
+       ->setAppId('https://shift-two.alwaysdata.net');
+
+$request = json_encode($server->generateRegisterRequest()->jsonSerialize());
+
+require_once 'html.php';

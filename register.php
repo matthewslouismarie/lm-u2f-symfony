2b03fd2f28ad2969061ec2435daf88da144f00b2
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-09 09:36:30 +0000

    Add LM utility classes and refine autoloader

--- register.php
+++ register.php
@@ -3,17 +3,34 @@
 require_once 'src/functions.php';
 
 spl_autoload_register(function ($class_name) {
-    $filename = basename(str_replace('\\', '/', $class_name)).'.php';
-    include 'src/'.$filename;
+    if ('Firehed' === substr($class_name, 0, 7)) {
+        $filename = basename(str_replace('\\', '/', $class_name)).'.php';
+        include 'src/'.$filename;
+    }
+});
+
+spl_autoload_register(function ($class_name) {
+    if ('LM' === substr($class_name, 0, 2)) {    
+        include 'src/'.str_replace('\\', '/', $class_name).'.php';
+    }
 });
 
 
 use Firehed\U2F\Server;
+use LM\Database\DatabaseConnection;
 
 $server = new Server();
 $server->disableCAVerification()
        ->setAppId('https://shift-two.alwaysdata.net');
 
 $request = json_encode($server->generateRegisterRequest()->jsonSerialize());
+
+$db_credentials = array(
+    'host' => 'mysql-shift-two.alwaysdata.net',
+    'db' => 'shift-two_hp',
+    'username' => '',
+    'password' => 'Utilisateurs MySQL'
+);
+DatabaseConnection::getInstance($db_credentials);
 
 require_once 'html.php';

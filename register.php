ebe40ceacc5d8cfb938bbd4f5e20c654fdf248de
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-09 09:49:31 +0000

    Move init code in init.php

--- register.php
+++ register.php
@@ -1,20 +1,6 @@
 <?php
 
-require_once 'src/functions.php';
-
-spl_autoload_register(function ($class_name) {
-    if ('Firehed' === substr($class_name, 0, 7)) {
-        $filename = basename(str_replace('\\', '/', $class_name)).'.php';
-        include 'src/'.$filename;
-    }
-});
-
-spl_autoload_register(function ($class_name) {
-    if ('LM' === substr($class_name, 0, 2)) {    
-        include 'src/'.str_replace('\\', '/', $class_name).'.php';
-    }
-});
-
+require 'private/init.php';
 
 use Firehed\U2F\Server;
 use LM\Database\DatabaseConnection;

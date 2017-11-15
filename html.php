4c8454617e12d1bf33664a0ff01967369d516f30
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-15 22:53:13 +0000

    Fix git ignoring .gitignore

--- html.php
+++ html.php
@@ -4,9 +4,18 @@
     <meta charset="utf-8">
     <script src="assets/u2f-api.js"></script>
     <script>
+      const completeRegistration = function fCompleteRegistration(registerResponse)
+      {
+        console.log(registerResponse);
+      }
       var request = <?= $request ?>;
+      var sign_requests = <?= $sign_requests ?>;
+      u2f.register([request], sign_requests, completeRegistration);
     </script>
   </head>
   <body>
+      <form action="#" method="post">
+        <input name="challenge" readonly type="text">
+      </form>
   </body>
 </html>

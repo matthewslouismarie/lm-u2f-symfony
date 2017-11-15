18379422ff3e53d5027f78694513ec84289c5103
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-15 23:11:16 +0000

    Writes signed challenge in input

--- html.php
+++ html.php
@@ -2,11 +2,13 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="assets/u2f-api.js"></script>
     <script>
       const completeRegistration = function fCompleteRegistration(registerResponse)
       {
         console.log(registerResponse);
+        $('#challenge').val(JSON.stringify(registerResponse));
       }
       var request = <?= $request ?>;
       var sign_requests = <?= $sign_requests ?>;
@@ -15,7 +17,7 @@
   </head>
   <body>
       <form action="#" method="post">
-        <input name="challenge" readonly type="text">
+        <input id="challenge" name="challenge" readonly type="text">
       </form>
   </body>
 </html>

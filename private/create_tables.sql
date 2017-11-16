1727d8d27b90f4caead56dc68cd318e010d1b765
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-16 00:10:38 +0000

    Set up database

--- private/create_tables.sql
+++ private/create_tables.sql
@@ -1,8 +1,18 @@
 # Todo: NF
 
-CREATE TABLE member
+CREATE OR REPLACE TABLE members
 (
-    id SMALLINT(4) UNSIGNED AUTO_INCREMENT KEY,
-    counter INT UNSIGNED NOT NULL
+    id SMALLINT UNSIGNED AUTO_INCREMENT KEY,
+    username CHAR(255) CHARACTER SET ascii COLLATE ascii_general_ci
 )
 ENGINE=InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE OR REPLACE TABLE u2f_authenticators
+(
+    id SMALLINT UNSIGNED AUTO_INCREMENT KEY,
+    member_id SMALLINT UNSIGNED REFERENCES members (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    counter SMALLINT UNSIGNED NOT NULL,
+    attestation VARCHAR(788) NOT NULL,-- @todo variable length, @todo in bytes?
+    public_key VARCHAR(88) NOT NULL,-- @todo should be 32 bytes, @todo in bytes
+    key_handle VARCHAR(88) NOT NULL -- @todo should be 255, @todo in bytes?
+)

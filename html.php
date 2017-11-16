6a87d1f84060191b17f823ab2dd5a45d3aa596a2
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-16 00:31:20 +0000

    Save registration details

--- html.php
+++ html.php
@@ -10,14 +10,17 @@
         console.log(registerResponse);
         $('#challenge').val(JSON.stringify(registerResponse));
       }
-      var request = <?= $request ?>;
+      var request = <?= $request_json ?>;
       var sign_requests = <?= $sign_requests ?>;
       u2f.register([request], sign_requests, completeRegistration);
     </script>
   </head>
   <body>
       <form action="#" method="post">
+        <input id="username" name="username" type="text" required>
+        <input id="request" name="request" readonly type="text" value="<?= htmlspecialchars($request_json, ENT_COMPAT, 'UTF-8', false) ?>">
         <input id="challenge" name="challenge" readonly type="text">
+        <button type="submit">Register</button>
       </form>
   </body>
 </html>

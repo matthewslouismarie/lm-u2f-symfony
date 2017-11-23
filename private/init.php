5ecf13262c032ce0e7a6452152eedb9e23cbbaa7
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-23 09:32:34 +0000

    Fix get_registrations_for_user(…)

--- private/init.php
+++ private/init.php
@@ -2,6 +2,8 @@
 
 require_once 'src/functions.php';
 
+use Firehed\U2F\Registration;
+
 spl_autoload_register(function ($class_name) {
     if ('Firehed' === substr($class_name, 0, 7)) {
         $filename = basename(str_replace('\\', '/', $class_name)).'.php';
@@ -17,13 +19,16 @@ spl_autoload_register(function ($class_name) {
 
 function get_registrations_for_user(int $member_id, \PDO $pdo): array
 {
-    $stmt = $pdo->prepare('SELECT counter FROM u2f_authenticators WHERE member_id = ?;');
+    $stmt = $pdo->prepare('SELECT counter, attestation, public_key, key_handle FROM u2f_authenticators WHERE member_id = ?;');
     $stmt->execute(array($member_id));
     $results = $stmt->fetchAll();
     $registrations = array();
     foreach ($results as $counter) {
         $registration = new Registration();
-        $registration->setCounter($counter);
+        $registration->setCounter($counter['counter']);
+        $registration->setAttestationCertificate($counter['attestation']);
+        $registration->setPublicKey(base64_decode($counter['public_key']));
+        $registration->setKeyHandle(base64_decode($counter['key_handle']));
         $registrations[] = $registration;
     }
     return $registrations;

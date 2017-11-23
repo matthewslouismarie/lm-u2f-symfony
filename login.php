41871ad3c96723be4e66b35453d99ed69aa65224
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-23 09:32:34 +0000

    Fix get_registrations_for_user(…)

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+
+session_start();
+
+require 'private/init.php';
+
+use Firehed\U2F\Server;
+use LM\Database\DatabaseConnection;
+use Firehed\U2F\RegisterResponse;
+
+$server = new Server();
+$server->disableCAVerification()
+       ->setAppId('https://shift-two.alwaysdata.net');
+
+if ('GET' === $_SERVER['REQUEST_METHOD']) {
+
+    $db_credentials = json_decode(file_get_contents('private/db.json'), true);
+    $pdo = DatabaseConnection::getInstance($db_credentials)->getPdo();
+
+    $registrations = get_registrations_for_user(2, $pdo);
+    $sign_requests = $server->generateSignRequests($registrations);
+    echo '<pre>';
+    var_dump($sign_requests);
+    echo '</pre>';
+
+    $_SESSION['registrations'] = serialize($registrations);
+    $_SESSION['sign_requests'] = serialize($sign_requests);
+    require_once 'login.html.php';
+} elseif ('POST' === $_SERVER['REQUEST_METHOD']) {
+    $request = unserialize($_SESSION['request']);
+    $username = $_POST['username'];
+    $server->setRegisterRequest($request);
+    $response = RegisterResponse::fromJson($_POST['challenge']);
+    $registration = $server->register($response);
+
+    $db_credentials = json_decode(file_get_contents('private/db.json'), true);
+
+    $pdo = DatabaseConnection::getInstance($db_credentials)->getPdo();
+
+    $pdo->beginTransaction();
+    $members_insert = $pdo->prepare('INSERT INTO members VALUES (NULL, :username)');
+    $members_insert->bindParam(':username', $username);
+    $success = $members_insert->execute();
+    $u2f_authenticators_insert = $pdo->prepare('INSERT INTO u2f_authenticators VALUES (NULL, :member_id, :counter, :attestation, :public_key, :key_handle)');
+    $u2f_authenticators_insert->bindParam(':member_id', $pdo->lastInsertId());
+    $u2f_authenticators_insert->bindParam(':attestation', base64_encode($registration->getAttestationCertificateBinary()));
+    $u2f_authenticators_insert->bindParam(':counter', $registration->getCounter());
+    $u2f_authenticators_insert->bindParam(':public_key', base64_encode($registration->getPublicKey()));
+    $u2f_authenticators_insert->bindParam(':key_handle', base64_encode($registration->getKeyHandleBinary()));
+    $u2f_authenticators_insert->execute();
+    $pdo->commit();
+?>
+Last insert id: <?= $pdo->lastInsertId() ?><br>
+Attestation: <?= base64_encode($registration->getAttestationCertificateBinary()) ?><br>
+Counter: <?= $registration->getCounter() ?><br>
+Public key: <?= base64_encode($registration->getPublicKey()) ?><br>
+Key handle: <?= base64_encode($registration->getKeyHandleBinary()) ?><br>
+<?php
+}

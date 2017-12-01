50cbd1d60893d4dca2e0e828547983750698496b
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-12-01 10:32:33 +0000

    Initialise Symfony 4 project

---
+++ tmp/register.php
@@ -0,0 +1,59 @@
+<?php
+
+session_start();
+
+require 'private/init.php';
+
+use Firehed\U2F\Server;
+use LM\Database\DatabaseConnection;
+use Firehed\U2F\RegisterResponse;
+
+$server = new Server();
+$server->disableCAVerification()
+       ->setAppId('https://shift-two.alwaysdata.net');
+
+if ('GET' === $_SERVER['REQUEST_METHOD']) {
+    $request = $server->generateRegisterRequest();
+    $reg_id = generate_reg_id();
+    $_SESSION[$reg_id]['register_request'] = serialize($request);
+    $request_json = json_encode($request); // ->jsonSerialize()?
+
+    $db_credentials = json_decode(file_get_contents('private/db.json'), true);
+
+    $pdo = DatabaseConnection::getInstance($db_credentials)->getPdo();
+
+    $registrations = get_registrations_for_user(0, $pdo); // @todo why do we need sign_requests for registration?
+    $sign_requests = json_encode($server->generateSignRequests($registrations, $reg_id));
+    require_once 'register.html.php';
+} elseif ('POST' === $_SERVER['REQUEST_METHOD']) {
+    $request = unserialize($_SESSION[$_POST['reg-id']]['register_request']);
+    unset($_SESSION[$_POST['reg-id']]);
+    $username = $_POST['username'];
+    $server->setRegisterRequest($request);
+    $response = RegisterResponse::fromJson($_POST['challenge']);
+    $registration = $server->register($response);
+
+    $db_credentials = json_decode(file_get_contents('private/db.json'), true);
+
+    $pdo = DatabaseConnection::getInstance($db_credentials)->getPdo();
+
+    $pdo->beginTransaction();
+    $members_insert = $pdo->prepare('INSERT INTO members VALUES (NULL, :username)');
+    $members_insert->bindParam(':username', $username);
+    $success = $members_insert->execute();
+    $u2f_authenticators_insert = $pdo->prepare('INSERT INTO u2f_authenticators VALUES (NULL, :member_id, :counter, :attestation, :public_key, :key_handle)');
+    $u2f_authenticators_insert->bindParam(':member_id', $pdo->lastInsertId());
+    $u2f_authenticators_insert->bindParam(':attestation', base64_encode($registration->getAttestationCertificateBinary()));
+    $u2f_authenticators_insert->bindParam(':counter', $registration->getCounter());
+    $u2f_authenticators_insert->bindParam(':public_key', base64_encode($registration->getPublicKey()));
+    $u2f_authenticators_insert->bindParam(':key_handle', base64_encode($registration->getKeyHandleBinary()));
+    $u2f_authenticators_insert->execute();
+    $pdo->commit();
+?>
+Last insert id: <?= $pdo->lastInsertId() ?><br>
+Attestation: <?= base64_encode($registration->getAttestationCertificateBinary()) ?><br>
+Counter:  <?= $registration->getCounter() ?><br>
+Public key: <?= base64_encode($registration->getPublicKey()) ?><br>
+Key handle: <?= base64_encode($registration->getKeyHandleBinary()) ?><br>
+<?php
+}

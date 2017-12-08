1e621231766232331d8e244fc6fee0c69727cdb6
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-12-08 16:35:20 +0000

    Implement the barebones of a controller for register view

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,11 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Firehed\U2F\Server;
+use LM\Database\DatabaseConnection;
+use App\Service\PDOService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class RegisterController
+class RegisterController extends AbstractController
 {
+    private $pdo;
+
+    public function __construct(PDOService $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     public function doGet(): Response
     {
-        return new Response("Hello");
+        $server = new Server();
+        $server->disableCAVerification()
+       ->setAppId('https://shift-two.alwaysdata.net');
+
+        $request = $server->generateRegisterRequest();
+        $reg_id = \Firehed\U2F\generate_reg_id();
+        $_SESSION[$reg_id]['register_request'] = serialize($request);
+        $request_json = json_encode($request); // ->jsonSerialize()?
+
+        $registrations = \firehed\u2f\get_registrations_for_user(0, $this->pdo->getPdo()); // @todo why do we need sign_requests for registration?
+        $sign_requests = json_encode($server->generateSignRequests($registrations, $reg_id));
+        return $this->render('template.twig');
     }
 }

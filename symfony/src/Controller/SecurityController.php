656ee98a5937ebcfa9e9e1b668ecbc0ed1b4e97f
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 00:32:36 +0100

    Enable basic form authentication using guards

--- symfony/src/Controller/SecurityController.php
+++ symfony/src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\LoginForm;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,27 +11,34 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends Controller
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="security_login")
      */
     public function login(Request $request, AuthenticationUtils $authUtils)
     {
         // get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
-
         // last username entered by the user
         $lastUsername = $authUtils->getLastUsername();
-
-        return $this->render('login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername,
+        ]);
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                'form' => $form->createView(),
+                'error' => $error,
+            )
+        );
     }
 
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="homepage")
      */
     public function home()
     {
-        return $this->render('home.html.twig');
+        ob_start();
+        var_dump($this->getUser());
+        $content = ob_get_clean();
+        return $this->render('home.html.twig', array('c' => $content));
     }
 }

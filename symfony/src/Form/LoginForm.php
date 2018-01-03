856431285bae988e8e669f12aab9881d98488096
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 18:08:03 +0100

    Set LoginForm's base class

--- symfony/src/Form/LoginForm.php
+++ symfony/src/Form/LoginForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\FormModel\LoginSubmission;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,8 +13,15 @@ class LoginForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('_username')
-            ->add('_password', PasswordType::class)
+            ->add('username')
+            ->add('password', PasswordType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => LoginSubmission::class,
+        ));
     }
 }

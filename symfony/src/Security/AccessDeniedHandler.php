4f35fcef1ddb94154bf9cc6574147bcebe713554
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 17:25:45 +0100

    Add AccessDeniedHandler

---
+++ symfony/src/Security/AccessDeniedHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function handle(Request $request, AccessDeniedException $exception)
+    {
+        return new RedirectResponse(
+            $this->router->generate('not_logged_out')
+        );
+    }
+}

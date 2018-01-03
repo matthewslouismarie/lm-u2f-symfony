3020efe5ca59f0fb3647b87859c01232e939988e
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 23:07:28 +0100

    @Assert\NotBlank() checks for null, empty string, and empty array

--- symfony/src/FormModel/U2fTokenUpdate.php
+++ symfony/src/FormModel/U2fTokenUpdate.php
@@ -7,7 +7,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 class U2fTokenUpdate
 {
     /**
-     * @todo check NotBlank() is the same as NotNull()
      * @Assert\NotBlank()
      */
     private $name;

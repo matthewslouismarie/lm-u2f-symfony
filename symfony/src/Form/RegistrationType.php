32785fcd558a8814faab26ef42370d895d8aae5d
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 23:23:59 +0100

    Rename RegistrationForm to RegistrationType

--- symfony/src/Form/RegistrationType.php
+++ symfony/src/Form/RegistrationType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RegistrationForm extends AbstractType
+class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {

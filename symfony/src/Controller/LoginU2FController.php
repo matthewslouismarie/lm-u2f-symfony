35490e90531039f43bce01863c8dc516257acab9
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-03 23:14:12 +0100

    Remove calls to Request::createFromGlobals()

--- symfony/src/Controller/LoginU2FController.php
+++ symfony/src/Controller/LoginU2FController.php
@@ -5,37 +5,35 @@ namespace App\Controller;
 use App\Service\AuthRequestService;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LoginU2FController extends AbstractController
+class LoginU2FController extends Controller
 {
     private $auth;
-    private $request;
 
     public function __construct(AuthRequestService $auth)
     {
         $this->auth = $auth;
-        $this->request = Request::createFromGlobals();        
     }
 
     /**
      * @Route("u2f-login", name="display_u2f_login", methods={"POST"})
      */
-    public function displayPage()
+    public function displayPage(Request $request)
     {
-        $auth_data = $this->auth->generate($this->request->get('username'));
+        $auth_data = $this->auth->generate($request->get('username'));
         return $this->render('u2f-login.html.twig', $auth_data);
     }
 
     /**
      * @Route("process-u2f-login", name="process_u2f_login", methods={"POST"})
      */
-    public function processU2FLogin()
+    public function processU2FLogin(Request $request)
     {
         $this->auth->processResponse(
-            $this->request->get('auth-id'),
-            $this->request->get('username'),
-            $this->request->get('response')
+            $request->get('auth-id'),
+            $request->get('username'),
+            $request->get('response')
         );
         return $this->render('successful-u2f-login.html.twig');
     }

5df532b333e864c0c7ba18ab5cbd07a11154198a
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-04 17:44:16 +0100

    Test protected pages

--- symfony/src/Form/LoginForm.php
+++ symfony/src/Form/LoginForm.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\FormModel\LoginSubmission;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,7 @@ class LoginForm extends AbstractType
         $builder
             ->add('username')
             ->add('password', PasswordType::class)
+            ->add('log in', SubmitType::class)
         ;
     }
 

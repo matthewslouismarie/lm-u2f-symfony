c0a92403d46b68ac11733598e423b2ef23cea421
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-04 22:50:54 +0100

    Create page to register first key (TKS)

--- symfony/tests/Controller/AccessManagement/AnonymousTest.php
+++ symfony/tests/Controller/AccessManagement/AnonymousTest.php
@@ -21,6 +21,7 @@ class Anonymous extends DbWebTestCase
         $this->checkUrlStatusCode('/', 200);
         $this->checkUrlStatusCode('/login', 200);
         $this->checkUrlStatusCode('/mkps/registration', 200);
+        $this->checkUrlStatusCode('/mkps/master-pair-first-key', 200);
     }
 
     public function testProtectedRoutes()

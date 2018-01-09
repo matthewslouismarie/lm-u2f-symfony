9fbc691995c15d2160eb8b06c1265414d077cee6
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-09 21:52:13 +0100

    Add UPUK authorizer route

---
+++ symfony/tests/Controller/UpukAuthorizerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Model\UserRequestedAction;
+
+class UpukAuthorizerTest extends DbWebTestCase
+{
+    private $session;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->session = $this
+            ->getContainer()
+            ->get('App\Service\SecureSessionService')
+        ;
+    }
+
+    public function testUpukAuthorizer()
+    {
+        $userRequestedAction = new UserRequestedAction(false, '/');
+        $sessionId = $this
+            ->session
+            ->store($userRequestedAction)
+        ;
+        $crawler = $this
+            ->getClient()
+            ->request('GET', '/all/u2f-authorization/upuk')
+        ;
+        $statusCode = $this
+            ->getClient()
+            ->getResponse()
+            ->getStatusCode()
+        ;
+        $this->assertEquals(200, $statusCode);
+    }
+}

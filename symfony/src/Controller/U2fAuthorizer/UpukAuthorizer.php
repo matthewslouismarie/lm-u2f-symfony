7c3525501694b4c2dbf352dbcf37038fec0b2062
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-10 16:03:22 +0100

    Add U2F form to UPUK authoriser

--- symfony/src/Controller/U2fAuthorizer/UpukAuthorizer.php
+++ symfony/src/Controller/U2fAuthorizer/UpukAuthorizer.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\U2fAuthorizer;
 
+use App\Form\U2fLoginType;
 use App\Form\UsernameAndPasswordType;
+use App\FormModel\U2fLoginSubmission;
 use App\FormModel\UsernameAndPasswordSubmission;
+use App\Service\AuthRequestService;
+use App\Service\SecureSessionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -24,14 +28,19 @@ class UpukAuthorizer extends AbstractController
      *  methods={"GET", "POST"},
      *  requirements={"sessionId"=".+"})
      */
-    public function upukUp(Request $request, string $sessionId)
+    public function upukUp(
+        Request $request,
+        SecureSessionService $sSession,
+        string $sessionId)
     {
-        $submission = new UsernameAndPasswordSubmission();
-        $form = $this->createForm(UsernameAndPasswordType::class, $submission);
+        $upSubmission = new UsernameAndPasswordSubmission();
+        $form = $this->createForm(UsernameAndPasswordType::class, $upSubmission);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $upSubmissionId = $sSession->store($upSubmission);
             $url = $this->generateUrl('u2f_authorization_upuk_uk', array(
                 'sessionId' => $sessionId,
+                'upSubmissionId' => $upSubmissionId,
             ));
             return new RedirectResponse($url);
         }
@@ -41,14 +50,41 @@ class UpukAuthorizer extends AbstractController
     }
 
     /**
+     * @todo What if the username doesn't exist, or doesn't have U2F tokens?
+     * 
      * @Route(
-     *  "/all/u2f-authorization/upuk/uk/{sessionId}",
+     *  "/all/u2f-authorization/upuk/uk/{sessionId}/{upSubmissionId}",
      *  name="u2f_authorization_upuk_uk",
      *  methods={"GET", "POST"},
-     *  requirements={"sessionId"=".+"})
+     *  requirements={"sessionId"=".+", "upSubmissionId"=".+"})
      */
-    public function upukUk()
+    public function upukUk(
+        AuthRequestService $auth,
+        Request $request,
+        SecureSessionService $sSession,
+        string $sessionId,
+        string $upSubmissionId)
     {
-        return $this->render('u2f_authorization/upuk/uk_login.html.twig');
+        $upSubmission = $sSession->getAndRemove($upSubmissionId);
+        if (!is_a($upSubmission, UsernameAndPasswordSubmission::class)) {
+            $url = $this->generateUrl('u2f_authorization_upuk_up', array(
+                'sessionId' => $sessionId,
+            ));
+            return new RedirectResponse($url);
+        }
+        $u2fData = $auth->generate($upSubmission->getUsername());
+        $u2fSubmission = new U2fLoginSubmission(
+            $upSubmission->getUsername(),
+            $upSubmission->getPassword(),
+            null,
+            $u2fData['auth_id']);
+        $form = $this->createForm(U2fLoginType::class, $u2fSubmission);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+        return $this->render('u2f_authorization/upuk/uk_login.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
--- symfony/tests/Controller/UpukAuthorizerTest.php
+++ symfony/tests/Controller/UpukAuthorizerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use App\Model\UserRequestedAction;
+use Firehed\U2F\SignRequest;
 
 class UpukAuthorizerTest extends DbWebTestCase
 {
@@ -45,5 +46,32 @@ class UpukAuthorizerTest extends DbWebTestCase
         $this->getClient()->followRedirect();
         $content = $this->getClient()->getResponse()->getContent();
         $this->assertContains('Please activate your U2F key.', $content);
+        
+        $session = $this
+            ->getContainer()
+            ->get('App\Service\SecureSessionService')
+        ;
+        $signRequests = array();
+        $signRequest = new SignRequest();
+        $signRequest->setAppId('https://172.16.238.10');
+        $signRequest->setChallenge('lXaq82clJBmXNnNWL1W6GA');
+        $signRequest->setKeyHandle(base64_decode('v8IplXz0zSQUXVYjvSWNcP/70AamVDoaROr1UcREnWaARrRABftdhhaKTFsYTgOj5CH6BUYxztAN9qrU3WcBZg=='));
+        $signRequests[1] = $signRequest;
+        $requestId = $session->store(serialize($signRequests));
+        $postUpLoginButton = $this
+            ->getClient()
+            ->getCrawler()
+            ->selectButton('u2f_login[submit]')
+        ;
+        $form = $postUpLoginButton->form(array(
+            'u2f_login[requestId]' => $requestId,
+            'u2f_login[u2fTokenResponse]' => '{"keyHandle":"v8IplXz0zSQUXVYjvSWNcP_70AamVDoaROr1UcREnWaARrRABftdhhaKTFsYTgOj5CH6BUYxztAN9qrU3WcBZg","clientData":"eyJ0eXAiOiJuYXZpZ2F0b3IuaWQuZ2V0QXNzZXJ0aW9uIiwiY2hhbGxlbmdlIjoibFhhcTgyY2xKQm1YTm5OV0wxVzZHQSIsIm9yaWdpbiI6Imh0dHBzOi8vMTcyLjE2LjIzOC4xMCIsImNpZF9wdWJrZXkiOiJ1bnVzZWQifQ","signatureData":"AQAAAIkwRgIhAN1YRiOqMs1fOCOm7MuOxfYJ6qN7A8PdXrhEzejtw3gNAiEAgi0JJmODYRTN8qflhBNsAjuDkJz06hTUZi2LNbaU4gk"}',
+        ));
+
+        $validateLogin = $this
+            ->getClient()
+            ->submit($form)
+        ;
+        $this->getClient()->followRedirect();
     }
 }

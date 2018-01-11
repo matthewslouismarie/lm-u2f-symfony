6966c0fa474ffb7f974d1de541d6d5b1ccae998c
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-11 15:34:15 +0100

    Create UUKP authoriser entry point

---
+++ symfony/tests/Controller/UukpAuthorizerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Model\AuthorizationRequest;
+
+class UukpAuthorizerTest extends AbstractAccessManagementTestCase
+{
+    private $sSession;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->sSession = $this
+            ->getContainer()
+            ->get('App\Service\SecureSessionService')
+        ;
+    }
+
+    public function testAuthorizer()
+    {
+        $this
+            ->getClient()
+            ->request('GET', '/not-authenticated/request-password-reset')
+        ;
+        $this
+            ->getClient()
+            ->followRedirect()
+        ;
+        $submitButton = $this
+            ->getClient()
+            ->getCrawler()
+            ->selectButton('username[submit]')
+        ;
+        $usernameForm = $submitButton->form(array(
+            'username[username]' => 'louis',
+        ));
+    }
+}

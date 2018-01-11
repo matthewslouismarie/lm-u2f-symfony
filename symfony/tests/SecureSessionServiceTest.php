2fd367fca6d234f483c0374a7b3f45cda52418e2
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-11 12:34:17 +0100

    Force objects stored in session to be serializable

---
+++ symfony/tests/SecureSessionServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Member;
+use App\Tests\Controller\AbstractAccessManagementTestCase;
+
+class SecureSessionServiceTest extends AbstractAccessManagementTestCase
+{
+    public function testSecureSessionService()
+    {
+        $sSession = self::$kernel
+            ->getContainer()
+            ->get('App\Service\SecureSessionService');
+        $member = new Member(null, 'louis');
+        $sid = $sSession->storeObject($member);
+    }
+}

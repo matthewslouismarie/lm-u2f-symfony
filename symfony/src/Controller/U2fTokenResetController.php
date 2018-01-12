007e24537ec9872f7cc3ab6d73d1e6d540223f72
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-12 14:45:38 +0100

    Fix resetU2fToken

--- symfony/src/Controller/U2fTokenResetController.php
+++ symfony/src/Controller/U2fTokenResetController.php
@@ -66,6 +66,7 @@ class U2fTokenResetController extends AbstractController
         $form = $this->createForm(U2fTokenRegistrationType::class, $submission);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $service->processResponse($submission->getU2fTokenResponse(), $this->getUser(), new DateTimeImmutable(), $submission->getRequestId());
             $u2fTokenToDelete = $om
                 ->getRepository(U2FToken::class)
                 ->getExcept(

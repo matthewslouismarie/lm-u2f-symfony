a1c0d49ff8be10c54a2f438fbf7fba83fd3269ae
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-16 16:04:03 +0100

    Refactor ValidCredentialValidator

--- symfony/src/Repository/MemberRepository.php
+++ symfony/src/Repository/MemberRepository.php
@@ -3,13 +3,44 @@
 namespace App\Repository;
 
 use App\Entity\Member;
+use App\Exception\InvalidPasswordException;
+use App\Exception\NonexistentMemberException;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MemberRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    private $upEncoder;
+
+    public function __construct(
+        RegistryInterface $registry,
+        UserPasswordEncoderInterface $upEncoder)
     {
         parent::__construct($registry, Member::class);
+        $this->upEncoder = $upEncoder;
+    }
+
+    public function getMember(string $username): Member
+    {
+        $member = $this->findOneBy([
+                'username' => $username,
+            ])
+        ;
+        if (null === $member) {
+            throw new NonexistentMemberException();
+        }
+        return $member;
+    }
+
+    public function checkPassword(Member $member, string $password): void
+    {
+        $isPasswordValid = $this
+            ->upEncoder
+            ->isPasswordValid($member, $password)
+        ;
+        if (false === $isPasswordValid) {
+            throw new InvalidPasswordException();
+        }
     }
 }
--- symfony/src/Validator/Constraints/ValidCredentialValidator.php
+++ symfony/src/Validator/Constraints/ValidCredentialValidator.php
@@ -2,68 +2,31 @@
 
 namespace App\Validator\Constraints;
 
-use App\Entity\Member;
+use App\Repository\MemberRepository;
 use App\Exception\InvalidPasswordException;
 use App\Exception\NonexistentMemberException;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ValidCredentialValidator extends ConstraintValidator
 {
-    private $om;
-    private $upEncoder;
+    private $memberRepository;
 
-    public function __construct(
-        ObjectManager $om,
-        UserPasswordEncoderInterface $upEncoder)
+    public function __construct(MemberRepository $memberRepository)
     {
-        $this->om = $om;
-        $this->upEncoder = $upEncoder;
+        $this->memberRepository = $memberRepository;
     }
 
     public function validate($up, Constraint $constraint)
     {
         try {
-            $member = $this->getMember($up->getUsername());
-            $this->checkPassword($member, $up->getPassword());
+            $member = $this->memberRepository->getMember($up->getUsername());
+            $this->memberRepository->checkPassword($member, $up->getPassword());
         } catch (NonexistentMemberException | InvalidPasswordException $e) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ string }}', $up->getUsername())
                 ->addViolation()
             ;
-        }
-    }
-
-    /**
-     * @todo Move in another class.
-     */
-    private function getMember(string $username): Member
-    {
-        $member = $this
-            ->om
-            ->getRepository(Member::class)->findOneBy([
-                'username' => $username,
-            ])
-        ;
-        if (null === $member) {
-            throw new NonexistentMemberException();
-        }
-        return $member;
-    }
-
-    /**
-     * @todo Move in another class.
-     */
-    private function checkPassword(Member $member, string $password): void
-    {
-        $isPasswordValid = $this
-            ->upEncoder
-            ->isPasswordValid($member, $password)
-        ;
-        if (false === $isPasswordValid) {
-            throw new InvalidPasswordException();
         }
     }
 }

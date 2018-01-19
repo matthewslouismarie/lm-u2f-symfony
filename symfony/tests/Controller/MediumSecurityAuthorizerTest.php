aecb907379d65b924ca395a8c89bbae0e03b0e99
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-19 00:47:42 +0000

    Create CredentialFiller

--- symfony/tests/Controller/MediumSecurityAuthorizerTest.php
+++ symfony/tests/Controller/MediumSecurityAuthorizerTest.php
@@ -3,84 +3,68 @@
 namespace App\Tests\Controller;
 
 use App\Form\Filler\LoginRequestFiller;
+use App\Form\Filler\CredentialFiller;
 use App\FormModel\U2fAuthenticationRequest;
 use Firehed\U2F\SignRequest;
 
-class MediumSecurityAuthorizerTest extends DbWebTestCase
+class MediumSecurityAuthorizerTest extends TestCaseTemplate
 {
     public function testCorrectLogin()
     {
         $this->logIn('louis', 'hello');
-        $this->assertTrue($this->getClient()->getResponse()->isRedirect());
-        $this->getClient()->followRedirect();
+        $this->assertIsRedirect();
+        $this->followRedirect();
+        $this->assertRegExp(
+            '/\/all\/u2f-authorization\/medium-security\/[a-z0-9]+/',
+            $this->getUri()
+        );
         $u2fButton = $this
-            ->getClient()
             ->getCrawler()
             ->selectButton('new_u2f_authentication[submit]')
         ;
-        $mock = $this->getContainer()->get('App\Service\MockU2fAuthenticationGenerator');
-        $cycle = $mock->getNewCycle();
+        $cycle = $this
+            ->getU2fAuthenticationMocker()
+            ->getNewCycle()
+        ;
         $u2fForm = $u2fButton->form([
             'new_u2f_authentication[u2fTokenResponse]' => $cycle->getResponse(),
         ]);
-        $submissionStack = $this->getContainer()->get('App\Service\SubmissionStack');
-        $pos = strrpos($this->getClient()->getRequest()->getUri(), '/');
-        $sid = substr($this->getClient()->getRequest()->getUri(), $pos + 1);
-        $submissionStack->set($sid, 2, $cycle->getRequest());
-        $this->getClient()->submit($u2fForm);
-        $this->assertTrue($this->getClient()->getResponse()->isRedirect());
-        $this->getClient()->followRedirect();
+        $sid = $this->getUriLastPart();
+        $this->getSubmissionStack()->set($sid, 2, $cycle->getRequest());
+        $this->submit($u2fForm);
+        $this->assertIsRedirect();
+        $this->followRedirect();
         $this->assertEquals(
             "http://localhost/not-authenticated/finalise-login/{$sid}",
-            $this->getClient()->getRequest()->getUri()
+            $this->getUri()
         );
         $loginRequestFiller = new LoginRequestFiller($this->getClient()->getCrawler());
-        $this->getClient()->submit($loginRequestFiller->getFilledForm());
+        $this->submit($loginRequestFiller->getFilledForm());
     }
 
     public function testIncorrectUsername()
     {
         $this->logIn('loui', 'hello');
-        $this->assertFalse($this->getClient()->getResponse()->isRedirect());
+        $this->assertIsNotRedirect();
     }
 
     public function testIncorrectPassword()
     {
         $this->logIn('louis', '');
-        $this->assertFalse($this->getClient()->getResponse()->isRedirect());
+        $this->assertIsNotRedirect();
     }
 
     public function testIncorrectCredentials()
     {
         $this->logIn('', '');
-        $this->assertFalse($this->getClient()->getResponse()->isRedirect());
+        $this->assertIsNotRedirect();
     }
 
     private function logIn(string $username, string $password): void
     {
-        $this
-            ->getClient()
-            ->request('GET', '/not-authenticated/start-login')
-        ;
-        $this->assertTrue($this->getClient()->getResponse()->isRedirect());
-        $this->getClient()->followRedirect();
-        $this->assertRegExp(
-            '/\/all\/u2f-authorization\/medium-security\/[a-z0-9]+/',
-            $this->getClient()->getRequest()->getUri()
-        );
-        $button = $this
-            ->getClient()
-            ->getCrawler()
-            ->selectButton('credential_authentication[submit]')
-        ;
-        $this->assertNotEquals(0, $button->count());
-        $form = $button->form([
-            'credential_authentication[username]' => $username,
-            'credential_authentication[password]' => $password,
-        ]);
-        $this
-            ->getClient()
-            ->submit($form)
-        ;
+        $this->doGet('/not-authenticated/start-login');
+        $this->followRedirect();
+        $formFiller = new CredentialFiller($this->getCrawler(), $username, $password);
+        $this->submit($formFiller->getFilledForm());
     }
 }

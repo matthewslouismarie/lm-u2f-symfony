9815316413628d88429b24f8566953e79d10f9df
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-01-25 10:07:28 +0000

    Add form to new registration's first step

--- symfony/tests/Controller/MemberRegistrationTest.php
+++ symfony/tests/Controller/MemberRegistrationTest.php
@@ -2,11 +2,20 @@
 
 namespace App\Tests\Controller;
 
+use App\Form\Filler\CredentialRegistrationFiller;
+
 class MemberRegistrationTest extends TestCaseTemplate
 {
     public function testRegistration(): void
     {
         $this->doGet('/not-authenticated/register');
         $this->assertEquals(200, $this->getHttpStatusCode());
+        $filler = new CredentialRegistrationFiller(
+            $this->getCrawler(),
+            'pwd',
+            'pwd',
+            'chat')
+        ;
+        $this->submit($filler->fillForm());
     }
 }

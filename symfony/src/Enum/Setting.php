ee6ec34cd0c906d6651f123b61101bcefbaf68fa
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-01 19:43:56 +0100

    Rename setting keys

--- symfony/src/Enum/Setting.php
+++ symfony/src/Enum/Setting.php
@@ -7,25 +7,25 @@ namespace App\Enum;
  */
 class Setting
 {
-    const N_U2F_KEYS_REG = "0";
+    const N_U2F_KEYS_REG = "N_U2F_KEYS_REG";
 
-    const N_U2F_KEYS_POST_AUTH = "1";
+    const N_U2F_KEYS_POST_AUTH = "N_U2F_KEYS_POST_AUTH";
 
-    const ALLOW_U2F_LOGIN = "2";
+    const ALLOW_U2F_LOGIN = "ALLOW_U2F_LGIN";
 
-    const SECURITY_STRATEGY = "3";
+    const SECURITY_STRATEGY = "SECURITY_STRATEGY";
 
-    const ALLOW_MEMBER_TO_MANAGE_U2F_KEYS = "4";
+    const ALLOW_MEMBER_TO_MANAGE_U2F_KEYS = "ALLOW_MEMBER_TO_MANAGE_U2F_KEYS";
 
-    const PWD_MIN_LENGTH = "5";
+    const PWD_MIN_LENGTH = "PWD_MIN_LENGTH";
 
-    const PWD_NUMBERS = "6";
+    const PWD_NUMBERS = "PWD_NUMBERS";
 
-    const PWD_SPECIAL_CHARS = "7";
+    const PWD_SPECIAL_CHARS = "PWD_SPECIAL_CHARS";
 
-    const PWD_UPPERCASE = "8";
+    const PWD_UPPERCASE = "PWD_UPPERCASE";
 
-    const PWD_ENFORCE_MIN_LENGTH = "9";
+    const PWD_ENFORCE_MIN_LENGTH = "PWD_ENFORCE_MIN_LENGTH";
 
     const USER_STUDY_MODE_ACTIVE = 'IS_USER_STUDY_MODE_ACTIVE';
 

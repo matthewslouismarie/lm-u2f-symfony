042b740bc35988f00c68fc2a75b34ebcfc545d85
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-10 21:59:28 +0100

    Replace grunt-contrib-sass by grunt-sass (faster and works on prod server)

--- symfony/Gruntfile.js
+++ symfony/Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['sass', 'watch']);
